93b63dd2ee271c801a8efab06fac7d7b1f8ee96c
hnsqls <2661108038@qq.com>
2024-08-27 09:38:13 +0800

    fiset init

---
+++ mianshidog/src/test/java/com/yupi/springbootinit/manager/CosManagerTest.java
@@ -0,0 +1,23 @@
+package com.ls.mianshidog.manager;
+
+import javax.annotation.Resource;
+
+import com.ls.mianshidog.manager.CosManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Cos 操作测试
+
+ */
+@SpringBootTest
+class CosManagerTest {
+
+    @Resource
+    private CosManager cosManager;
+
+    @Test
+    void putObject() {
+        cosManager.putObject("test", "test.json");
+    }
+}
